a1af7749e59d37e18c119108f4037d352c79bbdc
TAN-MAC <1240973173@qq.com>
2026-03-05 11:32:36 +0800

    <chore>(project/cmake): rename the project name, reupload the hole project, style the main.cpp

--- main/main.cpp
+++ main/main.cpp
@@ -6,13 +6,11 @@
 #include <nvs.h>
 #include <nvs_flash.h>
 
-// Tag for logging
 static const char *TAG = "main";
 #include "application.hpp"
 
 extern "C" void app_main(void)
 {
-    // Initialize NVS flash for WiFi configuration
     esp_err_t ret = nvs_flash_init();
     if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
         ESP_LOGW(TAG, "Erasing NVS flash to fix corruption");
@@ -21,7 +19,6 @@ extern "C" void app_main(void)
     }
     ESP_ERROR_CHECK(ret);
 
-    // Initialize and run the application
     auto &app = Application::GetInstance();
     app.Initialize();
     app.Run(); // This function runs the main event loop and never returns
